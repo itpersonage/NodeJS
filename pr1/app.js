4d4d6fad44c044f2b47abb637e1fe2b1ba422d0e
Andrey <it.personage@gmail.com>
2023-08-04 15:17:12 -0600

    lesson 1

--- pr1/app.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello, NPM!');
-
-const art = require('ascii-art');
-
-art.font("Hello,  NodeJS  APP!", 'Doom', (err, result) => {
-    if(!err){
-        console.log(result);
-    }
-});
--- pr3/app.mjs
+++
@@ -1,7 +0,0 @@
-import Square from './square.mjs'; // es module
-
-const mySq = new Square(3);
-
-console.log(`Area = ${mySq.area()}`);
-
-console.log(await mySq.someMethod());
