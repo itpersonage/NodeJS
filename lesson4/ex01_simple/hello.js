7ab8033afac0c4d707dcc5a94ee0a74f8c8af535
Andrey <it.personage@gmail.com>
2023-09-19 19:29:01 -0600

    lesson 4 50%

---
+++ lesson4/ex01_simple/hello.js
@@ -0,0 +1,5 @@
+const username = "Sergey";
+const sayHello = (name) => {
+    console.log(`Hello, $(name)`);
+};
+sayHello(username);
---
+++ lesson4/ex01_simple/hello.ts
@@ -0,0 +1,7 @@
+const username: string = "Sergey";
+
+const sayHello = (name: string) => {
+  console.log(`Hello, $(name)`);
+};
+
+sayHello(username);
